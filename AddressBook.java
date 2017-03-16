97e7528072f5da3d09b6ac4271c467436263c198
Stevensd99 <stevensd@countryday.net>
2017-03-16 09:41:54 -0400

    version 3.0
    -Derek Stevens

--- AddressBook.java
+++ AddressBook.java
@@ -7,39 +7,58 @@ public class AddressBook {
        private int numfriends; // Number of friends currently in AddressBook
 
     // Create an empty AddressBook
-	public AddressBook() {
-		friends = new Contact[10];
-		numfriends = 0;
+    public AddressBook() {
+        friends = new Contact[10];
+        numfriends = 0;
     }
-	public int numContacts(){
-		return Contacts.length
-	}
-	public int haveContact(String s){
-		return getName(s);
-	}
-	public int deleteContact(String s){
-		int pos = haveContact(s);
-		friends[pos] = friends[friends.length-1];
-		numfriends -1;
-		return haveContact(s);
-		
-	}
-	public static void main(String args[]){
-		Scanner s = new Scanner(System.in);
-		String choice = s.next();
-		System.out.println(menu())
-		if(choice == "add"){
-			System.out.println("What would you like to add? ");
-			String added = s.next();
-			addContact(added);
-		}
-		if(choice == "delete"){
-			System.out.println("What would you like to add? ");
-			String deleted = s.next();
-			deleteContact(deleted);
-		if(choice = "print"){
-			System.out.println(friends)
-		}
-			
-		}
-	}
+    public int numContacts(){
+        return numfriends;
+    }
+    public int haveContact(String s){
+        for(int in= 0; i<numfirends;i++){
+            if (friends[i].getName().equals(s)){
+                return i;
+            }
+            return -1;
+    }
+    }
+    public int deleteContact(String s){
+        int pos = haveContact(s);
+        friends[pos] = friends[friends.length-1];
+   
+       numfriends --;
+        return haveContact(s);
+        
+     }
+    public void printContacts(){
+        for(int i = 0; i<numfriends; i++){
+            System.out.println(friends[i]);
+        }
+    }
+    public void addContact(Contact c){
+        friends[numfriends] = c;
+        numfriends ++;
+    }
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        String choice = s.next();
+        Adressbook myBook = new Adressbook();
+        System.out.println(menu());
+        if(choice == "add"){
+            System.out.println("What would you like to add? ");
+            String added = s.next();
+            myBook.addContact(added);
+        }
+        if(choice == "delete"){
+            System.out.println("What would you like to add? ");
+            String deleted = s.next();
+            myBook.deleteContact(deleted);
+        }
+        if(choice = "print"){
+            myBook.printContacts();
+        }
+        if(choice == "number of friends"){
+            myBook.numContacts();
+        }
+        }
+    }
